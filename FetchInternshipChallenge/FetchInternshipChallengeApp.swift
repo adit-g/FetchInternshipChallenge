6f545dd27188c08c07aa00d8eaa8d4a2a31e3040
Adit G <dittykong5@gmail.com>
2023-09-30 18:00:19 -0500

    Initial Commit

---
+++ FetchInternshipChallenge/FetchInternshipChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchInternshipChallengeApp.swift
+//  FetchInternshipChallenge
+//
+//  Created by Adit G on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchInternshipChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
